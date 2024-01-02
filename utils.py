72f4d0fe0eeabbb87b1d1251113687a714859a9e
aticem <65057931+aticem@users.noreply.github.com>
2024-01-02 22:00:03 +0000

    pip install python-dotenav

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import pickle
 cv = pickle.load(open("models/cv.pkl", 'rb'))
 clf = pickle.load(open("models/clf.pkl", 'rb'))
 
-def model_predict(email):
+def make_prediction(email):
     if email == "":
         return ""
     tokenized_email = cv.transform([email]) # X 
